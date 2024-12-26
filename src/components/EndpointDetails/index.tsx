3efdc8d6f7bfc7b4c849b9c49feb3cdb69634088
darthrichlius <hello@rdieud.com>
2024-12-26 16:01:13 +0100

    fix: display well-formatted response body

--- src/components/EndpointDetails/index.tsx
+++ src/components/EndpointDetails/index.tsx
@@ -1,5 +1,6 @@
 // src/components/EndpointDetails.tsx
 import React from "react";
+import ReactJson from "@microlink/react-json-view";
 import DetailsTable from "./EndpointDetailsTable";
 import styles from "./styles.module.css";
 import EndpointDetailsParameterTable from "./EndpointDetailsParameterTable";
@@ -21,7 +22,7 @@ interface EndpointDetailsProps {
   protected: boolean;
   parameters?: EndpointParameterProps[];
   successResponseCode: number;
-  successResponseBody: Record<string, any>;
+  successResponseBody: string;
   errors?: EndpointErrorProps[];
 }
 
@@ -34,35 +35,58 @@ const EndpointDetails = ({
   successResponseCode,
   successResponseBody,
   errors,
-}: EndpointDetailsProps) => (
-  <div className={styles.endpointDetails}>
-    <h4>{title}</h4>
-    {description && <p>{description}</p>}
+}: EndpointDetailsProps) => {
+  let _successResponseBody;
+  try {
+    if (typeof successResponseBody === "string" && successResponseBody.length)
+      _successResponseBody = (
+        <ReactJson
+          name={false}
+          src={JSON.parse(successResponseBody)}
+          theme="rjv-default"
+          collapsed={false}
+          displayObjectSize={false}
+        />
+      );
+    else _successResponseBody = "-";
+  } catch (ee) {
+    throw new Error("Malformed json");
+  }
 
-    <DetailsTable
-      rows={[
-        { key: "Endpoint", value: endpoint },
-        { key: "Protected", value: isProtected ? "Yes" : "No" },
-        {
-          key: "Parameters",
-          value: parameters ? (
-            <EndpointDetailsParameterTable rows={parameters} />
-          ) : (
-            "None"
-          ),
-        },
-        { key: "Success - Response Code", value: successResponseCode },
-        {
-          key: "Success - Response Body",
-          value: <pre>{JSON.stringify(successResponseBody, null, 2)}</pre>,
-        },
-        {
-          key: "Errors",
-          value: errors ? <EndpointDetailsErrorTable rows={errors} /> : "None",
-        },
-      ]}
-    />
-  </div>
-);
+  return (
+    <div className={styles.endpointDetails}>
+      <h4>{title}</h4>
+      {description && <p>{description}</p>}
+
+      <DetailsTable
+        rows={[
+          { key: "Endpoint", value: endpoint },
+          { key: "Protected", value: isProtected ? "Yes" : "No" },
+          {
+            key: "Parameters",
+            value: parameters ? (
+              <EndpointDetailsParameterTable rows={parameters} />
+            ) : (
+              "None"
+            ),
+          },
+          { key: "Success - Response Code", value: successResponseCode },
+          {
+            key: "Success - Response Body",
+            value: _successResponseBody,
+          },
+          {
+            key: "Errors",
+            value: errors ? (
+              <EndpointDetailsErrorTable rows={errors} />
+            ) : (
+              "None"
+            ),
+          },
+        ]}
+      />
+    </div>
+  );
+};
 
 export default EndpointDetails;
